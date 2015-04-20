7c3a51f11e05ae2b3056b322cd4c8f134d2c1727
Ken West <ken.west@internode.on.net>
2015-04-21 09:08:09 +1000

    CRM-16324 Scheduled jobs won't accept zero-valued parameters

--- CRM/Core/ScheduledJob.php
+++ CRM/Core/ScheduledJob.php
@@ -1,9 +1,9 @@
 <?php
 /*
  +--------------------------------------------------------------------+
- | CiviCRM version 4.4                                                |
+ | CiviCRM version 4.6                                                |
  +--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2013                                |
+ | Copyright CiviCRM LLC (c) 2004-2015                                |
  +--------------------------------------------------------------------+
  | This file is a part of CiviCRM.                                    |
  |                                                                    |
@@ -23,14 +23,14 @@
  | GNU Affero General Public License or the licensing of CiviCRM,     |
  | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
  +--------------------------------------------------------------------+
-*/
+ */
 
 /**
  * This interface defines methods that need to be implemented
  * by every scheduled job (cron task) in CiviCRM.
  *
  * @package CRM
- * @copyright CiviCRM LLC (c) 2004-2013
+ * @copyright CiviCRM LLC (c) 2004-2015
  * $Id$
  *
  */
@@ -44,14 +44,9 @@ class CRM_Core_ScheduledJob {
 
   var $remarks = array();
 
-  /*
-     * Class constructor
-     *
-     * @param string $namespace namespace prefix for component's files
-     * @access public
-     *
-     */
-
+  /**
+   * @param array $params
+   */
   public function __construct($params) {
     foreach ($params as $name => $param) {
       $this->$name = $param;
@@ -72,7 +67,7 @@ class CRM_Core_ScheduledJob {
 
       foreach ($lines as $line) {
         $pair = explode("=", $line);
-        if (empty($pair[0]) || empty($pair[1])) {
+        if ($pair === false || count($pair) != 2 || trim($pair[0]) == '' || trim($pair[1]) == '') {
           $this->remarks[] .= 'Malformed parameters!';
           break;
         }
@@ -81,13 +76,19 @@ class CRM_Core_ScheduledJob {
     }
   }
 
+  /**
+   * @param null $date
+   */
   public function saveLastRun($date = NULL) {
-    $dao           = new CRM_Core_DAO_Job();
-    $dao->id       = $this->id;
+    $dao = new CRM_Core_DAO_Job();
+    $dao->id = $this->id;
     $dao->last_run = ($date == NULL) ? CRM_Utils_Date::currentDBDate() : CRM_Utils_Date::currentDBDate($date);
     $dao->save();
   }
 
+  /**
+   * @return bool
+   */
   public function needsRunning() {
     // run if it was never run
     if (empty($this->last_run)) {
@@ -100,7 +101,7 @@ class CRM_Core_ScheduledJob {
         return TRUE;
 
       case 'Hourly':
-        $now     = CRM_Utils_Date::currentDBDate();
+        $now = CRM_Utils_Date::currentDBDate();
         $hourAgo = strtotime('-1 hour', strtotime($now));
         $lastRun = strtotime($this->last_run);
         if ($lastRun < $hourAgo) {
@@ -108,8 +109,8 @@ class CRM_Core_ScheduledJob {
         }
 
       case 'Daily':
-        $now     = CRM_Utils_Date::currentDBDate();
-        $dayAgo  = strtotime('-1 day', strtotime($now));
+        $now = CRM_Utils_Date::currentDBDate();
+        $dayAgo = strtotime('-1 day', strtotime($now));
         $lastRun = strtotime($this->last_run);
         if ($lastRun < $dayAgo) {
           return TRUE;
@@ -119,6 +120,7 @@ class CRM_Core_ScheduledJob {
     return FALSE;
   }
 
-  public function __destruct() {}
-}
+  public function __destruct() {
+  }
 
+}
